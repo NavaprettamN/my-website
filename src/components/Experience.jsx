fec5b2686fecf6d35ee42179b4bbd25f2793bade
NavaprettamN <navaprettam.n214@gmail.com>
2023-11-11 20:16:07 +0530

    experience update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,55 +1,63 @@
 import React from 'react'
 
 import trail_image from '../assets/trail_image.jpg'
+import html_img from "../assets/html_img.png"
+import css_img from "../assets/css_img.png"
+import js_img from "../assets/js_img.png"
+import python_img from "../assets/python_img.png"
+import java_img from "../assets/java_img.png"
+import tailwind_img from "../assets/tailwind_img.png"
+import github_img from "../assets/github_img.png"
+import react_img from "../assets/react_img.png"
 
 const Experience = () => {
 
     const techs = [
         {
             id: 1,
-            src: trail_image,
+            src: html_img,
             title: 'HTML',
             style: 'shadow-orange-500'
         },
         {
             id: 2,
-            src: trail_image,
+            src: css_img,
             title: 'CSS',
             style: 'shadow-blue-500'
         },
         {
             id: 3,
-            src: trail_image,
+            src: js_img,
             title: 'Javascript',
             style: 'shadow-yellow-500'
         },
         {
             id: 4,
-            src: trail_image,
+            src: react_img,
             title: 'React',
             style: 'shadow-purple-800'
         },
         {
             id: 5,
-            src: trail_image,
+            src: tailwind_img,
             title: 'Tailwind',
             style: 'shadow-sky-200'
         },
         {
             id: 6,
-            src: trail_image,
-            title: 'NEXT JS',
+            src: java_img,
+            title: 'Java',
             style: 'shadow-green-100'
         },
         {
             id: 7,
-            src: trail_image,
+            src: github_img,
             title: 'GitHub',
             style: 'shadow-pink-200'
         },
         {
             id: 8,
-            src: trail_image,
+            src: python_img,
             title: 'Python',
             style: 'shadow-blue-200'
         },
@@ -63,14 +71,14 @@ const Experience = () => {
                 <p className='py-6'>Something to check</p>
             </div>
 
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 md:gap-x-40  gap-8 text-center py-8 px-12 sm:px-0'>
 
                 {
                     techs.map(({id, src, title, style}) => (
-                        <div key={id} className={`shadow-md hover:scale-105 duration-300 py-2 rounded-lg ${style}`}>
-                    <img src={src} alt="" className='w-20 mx-auto'/>
-                    <p className='mt-4'>{title}</p>
-                </div>
+                        <div key={id} className={`shadow-md flex flex-col hover:scale-105 duration-300 py-auto rounded-lg ${style}`}>
+                            <img src={src} alt="" className='w-auto mx-auto h-12'/>
+                            <p className='mt-4'>{title}</p>
+                        </div>
                     ))
                 }
 
